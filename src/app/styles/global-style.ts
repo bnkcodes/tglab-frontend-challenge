aaa558bf1b2e9a0a1138c8048114f83729de6f08
Bruno Neckel <bruno@bnkcodes.com>
2025-09-22 23:56:39 -0300

    chore: move fonts to public and fix GlobalStyle paths

--- src/app/styles/global-style.ts
+++ src/app/styles/global-style.ts
@@ -3,126 +3,126 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
 	@font-face {
 		font-family: 'Robert Sans';
-		src: url('./src/assets/fonts/robert-sans/RobertSans-Thin.ttf') format('truetype');
+		src: url('/fonts/robert-sans/RobertSans-Thin.ttf') format('truetype');
 		font-weight: 100;
 		font-style: normal;
 	}
 
 	@font-face {
 		font-family: 'Robert Sans';
-		src: url('./src/assets/fonts/robert-sans/RobertSans-ThinItalic.ttf') format('truetype');
+		src: url('/fonts/robert-sans/RobertSans-ThinItalic.ttf') format('truetype');
 		font-weight: 100;
 		font-style: italic;
 	}
 
 	@font-face {
 		font-family: 'Robert Sans';
-		src: url('./src/assets/fonts/robert-sans/RobertSans-ExtraLight.ttf') format('truetype');
+		src: url('/fonts/robert-sans/RobertSans-ExtraLight.ttf') format('truetype');
 		font-weight: 200;
 		font-style: normal;
 	}
 
 	@font-face {
 		font-family: 'Robert Sans';
-		src: url('./src/assets/fonts/robert-sans/RobertSans-ExtraLightItalic.ttf') format('truetype');
+		src: url('/fonts/robert-sans/RobertSans-ExtraLightItalic.ttf') format('truetype');
 		font-weight: 200;
 		font-style: italic;
 	}
 
 	@font-face {
 		font-family: 'Robert Sans';
-		src: url('./src/assets/fonts/robert-sans/RobertSans-Light.ttf') format('truetype');
+		src: url('/fonts/robert-sans/RobertSans-Light.ttf') format('truetype');
 		font-weight: 300;
 		font-style: normal;
 	}
 
 	@font-face {
 		font-family: 'Robert Sans';
-		src: url('./src/assets/fonts/robert-sans/RobertSans-LightItalic.ttf') format('truetype');
+		src: url('/fonts/robert-sans/RobertSans-LightItalic.ttf') format('truetype');
 		font-weight: 300;
 		font-style: italic;
 	}
 
 	@font-face {
 		font-family: 'Robert Sans';
-		src: url('./src/assets/fonts/robert-sans/RobertSans-Regular.ttf') format('truetype');
+		src: url('/fonts/robert-sans/RobertSans-Regular.ttf') format('truetype');
 		font-weight: 400;
 		font-style: normal;
 	}
 
 	@font-face {
 		font-family: 'Robert Sans';
-		src: url('./src/assets/fonts/robert-sans/RobertSans-Italic.ttf') format('truetype');
+		src: url('/fonts/robert-sans/RobertSans-Italic.ttf') format('truetype');
 		font-weight: 400;
 		font-style: italic;
 	}
 
 	@font-face {
 		font-family: 'Robert Sans';
-		src: url('./src/assets/fonts/robert-sans/RobertSans-Medium.ttf') format('truetype');
+		src: url('/fonts/robert-sans/RobertSans-Medium.ttf') format('truetype');
 		font-weight: 500;
 		font-style: normal;
 	}
 
 	@font-face {
 		font-family: 'Robert Sans';
-		src: url('./src/assets/fonts/robert-sans/RobertSans-MediumItalic.ttf') format('truetype');
+		src: url('/fonts/robert-sans/RobertSans-MediumItalic.ttf') format('truetype');
 		font-weight: 500;
 		font-style: italic;
 	}
 
 	@font-face {
 		font-family: 'Robert Sans';
-		src: url('./src/assets/fonts/robert-sans/RobertSans-SemiBold.ttf') format('truetype');
+		src: url('/fonts/robert-sans/RobertSans-SemiBold.ttf') format('truetype');
 		font-weight: 600;
 		font-style: normal;
 	}
 
 	@font-face {
 		font-family: 'Robert Sans';
-		src: url('./src/assets/fonts/robert-sans/RobertSans-SemiBoldItalic.ttf') format('truetype');
+		src: url('/fonts/robert-sans/RobertSans-SemiBoldItalic.ttf') format('truetype');
 		font-weight: 600;
 		font-style: italic;
 	}
 
 	@font-face {
 		font-family: 'Robert Sans';
-		src: url('./src/assets/fonts/robert-sans/RobertSans-Bold.ttf') format('truetype');
+		src: url('/fonts/robert-sans/RobertSans-Bold.ttf') format('truetype');
 		font-weight: 700;
 		font-style: normal;
 	}
 
 	@font-face {
 		font-family: 'Robert Sans';
-		src: url('./src/assets/fonts/robert-sans/RobertSans-BoldItalic.ttf') format('truetype');
+		src: url('/fonts/robert-sans/RobertSans-BoldItalic.ttf') format('truetype');
 		font-weight: 700;
 		font-style: italic;
 	}
 
 	@font-face {
 		font-family: 'Robert Sans';
-		src: url('./src/assets/fonts/robert-sans/RobertSans-ExtraBold.ttf') format('truetype');
+		src: url('/fonts/robert-sans/RobertSans-ExtraBold.ttf') format('truetype');
 		font-weight: 800;
 		font-style: normal;
 	}
 
 	@font-face {
 		font-family: 'Robert Sans';
-		src: url('./src/assets/fonts/robert-sans/RobertSans-ExtraBoldItalic.ttf') format('truetype');
+		src: url('/fonts/robert-sans/RobertSans-ExtraBoldItalic.ttf') format('truetype');
 		font-weight: 800;
 		font-style: italic;
 	}
 
 	@font-face {
 		font-family: 'Robert Sans';
-		src: url('./src/assets/fonts/robert-sans/RobertSans-Black.ttf') format('truetype');
+		src: url('/fonts/robert-sans/RobertSans-Black.ttf') format('truetype');
 		font-weight: 900;
 		font-style: normal;
 	}
 
 	@font-face {
 		font-family: 'Robert Sans';
-		src: url('./src/assets/fonts/robert-sans/RobertSans-BlackItalic.ttf') format('truetype');
+		src: url('/fonts/robert-sans/RobertSans-BlackItalic.ttf') format('truetype');
 		font-weight: 900;
 		font-style: italic;
 	}
